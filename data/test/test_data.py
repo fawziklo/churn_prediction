f06ed83d3f3109204d40b2bb7ab6241464a69373
R.Fawzi <fawzi.rida@hotmail.com>
2021-04-24 13:12:23 +0200

    add unit testing to pandas csv lecture

--- data/test/test_data.py
+++ data/test/test_data.py
@@ -11,11 +11,12 @@ def data():
 
 @pytest.mark.mandatory
 def test_columns_names(data):
-    dt.validate(
-        data.columns,
-        {'customerID', 'gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure', 'PhoneService', 'MultipleLines',
-         'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV',
-         'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod', 'MonthlyCharges', 'TotalCharges', 'Churn'})
+    real_column_names = {'customerID', 'gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure', 'PhoneService',
+                         'MultipleLines','InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection',
+                         'TechSupport','StreamingTV','StreamingMovies', 'Contract', 'PaperlessBilling',
+                         'PaymentMethod', 'MonthlyCharges','TotalCharges', 'Churn'}
+
+    dt.validate(data.columns, real_column_names)
 
 
 if __name__ == '__main__':
